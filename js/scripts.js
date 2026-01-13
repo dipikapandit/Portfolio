faf7e6ddc9bf806909c524ac9e63a51d12bd4dd5
Dipika Pandit <dipika.815421@pmc.tu.edu.np>
2026-01-13 17:33:57 +0545

    added interaction in the design gallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Logo hover effect
+
 function hoverEffect(element) {
     element.style.transform = "scale(1.25)";
     element.style.transition = "transform 0.3s ease";
@@ -18,3 +20,25 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+
+// Slant items click effect
+
+function handleClick(event) {
+    event.style.transform = 'scale(0.9)';
+    event.style.transition = 'transform 1s ease';
+}
+function resetClick(event) {
+    event.style.transform = 'scale(1)';
+}
+
+const slantItems = document.getElementsByClassName('slant-item');
+for (let i = 0; i < slantItems.length; i++) {
+    slantItems[i].onclick = function() {
+        handleClick(this);
+    }
+    slantItems[i].ontransitionend = function() {
+        resetClick(this);
+    }
+}
